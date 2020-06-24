088aa22cb399e019b3047e420eab685cc73f2d7f
treezstephen <stephen@treez.io>
2020-06-23 23:05:00 -0700

    Setup Hapi

--- manga-service/src/server/index.ts
+++ manga-service/src/server/index.ts
@@ -1,3 +1,33 @@
-import path from 'path';
+import * as Hapi from '@hapi/hapi';
 
-console.log(`Hello World!`)
+const host = "localhost";
+const port = 8001;
+
+const server: Hapi.Server = new Hapi.Server({
+    host,
+    port,
+})
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
+
+        return 'Hello World!';
+    }
+});
+
+const init = async () => {
+    try {
+        await server.start();
+        
+        
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log( `Server running @ ${ server.info.uri }` );
+}
+
+init();
